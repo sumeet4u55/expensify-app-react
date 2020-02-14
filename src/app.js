ab80b663e4c97f1ad5e04439a06b9a448fa25feb
Sumeet Bagewadi <sumeet.bagewadi@sap.com>
2020-02-15 00:21:14 +0530

    New wewbpack for CSS loader

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configStore from './store/configureStore';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import {Provider} from 'react-redux';
+import 'react-dates/lib/css/_datepicker.css';
 
 const store = configStore();
 
