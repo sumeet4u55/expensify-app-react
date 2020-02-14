61d43ad0b5c177ddca0d9f1c158dd45cb65f1698
Sumeet Bagewadi <sumeet.bagewadi@sap.com>
2020-02-15 01:26:46 +0530

    eroku error

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, '..', '/public');
+const publicPath = path.join(__dirname, '..', 'public/');
 
 app.use(express.static(publicPath));
 
